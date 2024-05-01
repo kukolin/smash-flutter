0c33ff320e8408e87f5855fed3fec01458504735
Lucas Anezin <lucas.anezin@gmail.com>
2024-04-30 22:15:45 -0300

    Crea y guarda un user id

--- lib/domain/presenter/home_screen/home_screen_view_model.dart
+++ lib/domain/presenter/home_screen/home_screen_view_model.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smash_flutter/firebase_service.dart';
+import 'package:uuid/uuid.dart';
 
 class HomeScreenViewModel extends ChangeNotifier{
 
@@ -9,6 +11,14 @@ class HomeScreenViewModel extends ChangeNotifier{
 
   HomeScreenViewModel(this.firebaseService);
 
-  void onWidgetInitialize() {
+  void onWidgetInitialize() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.getString('myId') ?? _generateIdAndSave(prefs);
+  }
+
+  _generateIdAndSave(SharedPreferences prefs) async {
+    var uuid = const Uuid().v4();
+    debugPrint("creado id: ${uuid.substring(24)}");
+    await prefs.setString('myId', uuid.substring(24));
   }
 }
